0756e4d48264daa3f5e1f2a0c092a083edb96eed
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 14:26:04 -0300

    Create about.js

---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import imgMario from "./assets/claudio-luiz-castro-mario.jpg";
+
+const about = /* html */ `
+  <h2>About</h2>
+  <p>
+    Mario's Ristorante is a plumber-run restaurant rooted in traditional
+    Mushroom Kindom's cooking and time-honored recipes. Our focus is simple:
+    quality ingredients, classic techniques, and a warm, welcoming table for
+    everyone (except Bowser).
+  </p>
+  <img
+    src=${imgMario}
+    alt="Epic Super Mario toy on table cheering"
+    width="640"
+    height="426"
+  />
+`;
+
+export { about };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { home } from "./components/home";
 import { menu } from "./components/menu";
+import { about } from "./components/about";
 
 const content = document.querySelector("#content");
 
