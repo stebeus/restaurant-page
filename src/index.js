0006f4756d552d0b6e90f1a53507baf2248f23dc
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 11:26:28 -0300

    Create heading element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { createNewElement } from "./util";
 
 const content = document.querySelector("#content");
+
+const heading = createNewElement("h1", {}, "Mario's Ristorante");
+
+content.append(heading);
