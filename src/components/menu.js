298268f64dbac0b1eacc2031271d6f20e5231227
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 14:25:23 -0300

    Create menu.js

---
+++ src/components/menu.js
@@ -0,0 +1,32 @@
+import imgMushroomPizza from "./assets/david-foodphototasty.jpg";
+import imgSpaghetti from "./assets/diego-ramirez.jpg";
+
+const menu = /* html */ `
+  <h2>Menu</h2>
+  <h3>Mushroom Pizza</h3>
+  <p>
+    Our Mushroom Pizza features a light, golden crust topped with classic tomato
+    sauce, melted mozzarella, and freshly sliced mushrooms. Simple and aromatic,
+    it highlights the earthy flavor of mushrooms in every bite.
+  </p>
+  <img
+    src=${imgMushroomPizza}
+    alt="Brown mushroom pizza"
+    width="640"
+    height="640"
+  />
+  <h3>Spaghetti</h3>
+  <p>
+    The Spaghetti is served with slow-simmered tomato sauce, tender meatballs,
+    and a generous topping of grated cheese. Finished with fresh basil, it
+    delivers a rich, comforting taste rooted in Italian tradition.
+  </p>
+  <img
+    src=${imgSpaghetti}
+    alt="Italian spaghetti with meatballs and granulated cheese on top"
+    width="640"
+    height="640"
+  />
+`;
+
+export { menu };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import { createNewElement } from "./util";
-import pizzaImg from "./assets/fabrizio-pullara.jpg";
+import { home } from "./components/home";
+import { menu } from "./components/menu";
 
 const content = document.querySelector("#content");
 
-const heading = createNewElement("h1", {}, "Mario's Ristorante");
-const subheading = createNewElement("h2", {}, "Ciao, cliente!");
-const bodyText = createNewElement(
-  "p",
-  {},
-  "Welcome to Mario's Ristorante, where classic Italian flavors meet a warm, family-run atmosphere. Inspired by traditional recipes and simple, honest ingredients, our kitchen serves timeless dishes crafted with care!"
-);
-const picture = createNewElement("img", {
-  src: pizzaImg,
-  alt: "Italian pizza getting removed from the wood-fired oven",
-  width: "640",
-  height: "426",
-});
-
-content.append(heading, subheading, bodyText, picture);
+content.innerHTML = home;
