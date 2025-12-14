46487086f09a38218316f6fc0f7dd47f544adb2a
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 11:28:12 -0300

    Create body text element

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ const content = document.querySelector("#content");
 
 const heading = createNewElement("h1", {}, "Mario's Ristorante");
 const subheading = createNewElement("h2", {}, "Ciao, cliente!");
+const bodyText = createNewElement(
+  "p",
+  {},
+  "Welcome to Mario's Ristorante, where classic Italian flavors meet a warm, family-run atmosphere. Inspired by traditional recipes and simple, honest ingredients, our kitchen serves timeless dishes crafted with care!"
+);
 
-content.append(heading, subheading);
+content.append(heading, subheading, bodyText);
