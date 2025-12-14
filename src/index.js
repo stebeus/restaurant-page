da59b4190d6e3ccf1aebb27235c73c2e61d0370c
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 11:27:10 -0300

    Create subheading element

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { createNewElement } from "./util";
 const content = document.querySelector("#content");
 
 const heading = createNewElement("h1", {}, "Mario's Ristorante");
+const subheading = createNewElement("h2", {}, "Ciao, cliente!");
 
-content.append(heading);
+content.append(heading, subheading);
