e230065d55b832058f65f90c6233f7af28ade1c2
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-14 14:23:52 -0300

    Create the home module file

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import imgPizza from "./assets/fabrizio-pullara.jpg";
+
+const home = /* html */ `
+  <h1>Mario's Ristorante</h1>
+  <h2>Home</h2>
+  <p>
+    Welcome to Mario's Ristorante, where classic Italian flavors meet a warm,
+    family-run atmosphere. Inspired by traditional recipes and simple, honest
+    ingredients, our kitchen serves timeless dishes crafted with care!
+  </p>
+  <img
+    src=${imgPizza}
+    alt="Italian pizza getting removed from the wood-fired oven"
+    width="640"
+    height="420"
+  />
+`;
+
+export { home };
